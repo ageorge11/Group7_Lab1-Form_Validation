f2140a991324b05651a36d46449bfd673406389c
ageorge11 <ageorg11@confederationcollege.ca>
2020-10-11 09:56:50 -0400

    commiting the last seven fields with req styles and scripts

--- script.js
+++ script.js
@@ -60,3 +60,74 @@ function validateDate(dob) {
     }    
 }
 
+//validating the password entered as per the requirement
+function ValidatePassword(psw) {
+    // Validate lowercase letters
+  var lowerCaseLetters = /[a-z]/g;
+  if(myInput.value.match(lowerCaseLetters)) {  
+    letter.classList.remove("invalid");
+    letter.classList.add("valid");
+  } else {
+    letter.classList.remove("valid");
+    letter.classList.add("invalid");
+  }
+  
+  // Validate capital letters
+  var upperCaseLetters = /[A-Z]/g;
+  if(myInput.value.match(upperCaseLetters)) {  
+    capital.classList.remove("invalid");
+    capital.classList.add("valid");
+  } else {
+    capital.classList.remove("valid");
+    capital.classList.add("invalid");
+  }
+
+  // Validate numbers
+  var numbers = /[0-9]/g;
+  if(myInput.value.match(numbers)) {  
+    number.classList.remove("invalid");
+    number.classList.add("valid");
+  } else {
+    number.classList.remove("valid");
+    number.classList.add("invalid");
+  }
+  
+  // Validate length
+  if(myInput.value.length >= 8) {
+    length.classList.remove("invalid");
+    length.classList.add("valid");
+  } else {
+    length.classList.remove("valid");
+    length.classList.add("invalid");
+  }
+}
+
+//validating the password re-entered
+function ValidateConfirmPassword()
+{
+    if (document.getElementById('psw').value ==
+    document.getElementById('rpass').value) {
+    document.getElementById('cmessage').style.color = 'green';
+    document.getElementById('cmessage').innerHTML = 'matching';
+  } else {
+    document.getElementById('cmessage').style.color = 'red';
+    document.getElementById('cmessage').innerHTML = 'not matching';
+  }
+
+}
+
+//validating the format of file uploaded
+function Checkfiles()
+    {
+        var fileName = document.getElementById('fileid').value;
+        var ext = fileName.substring(fileName.lastIndexOf('.') + 1);
+
+    if(ext =="word" || ext=="pdf")
+    {
+        return true;
+    }
+    else
+    {
+        alert("Upload word or pdf files only");
+    }
+    }
