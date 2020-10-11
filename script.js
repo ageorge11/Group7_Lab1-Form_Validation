2578294ffb560ab48970280b9d221a3002216fd8
clinseeldhose <clinseeldhose@gmail.com>
2020-10-11 09:49:31 -0400

    adding fields from 8-15 in the registration with req script and style

--- script.js
+++ script.js
@@ -27,3 +27,36 @@ function checkForNumbers(input) {
 
 }
 
+//script to display company name and year of experience depending on the answer for experience
+function checkExp(jobvalue) {
+
+    if(jobvalue != "yes")
+    {
+        document.getElementById("expCheck").style.display = "none";
+    }
+    else{
+        document.getElementById("expCheck").style.display = "block";
+    }
+}
+
+//validation for date of birth
+function validateDate(dob) {
+    
+    var today = new Date();
+    var inputDate = dob.value;
+    //console.log(today);
+    //console.log(inputDate);
+    var parts = inputDate.split("-");
+    var year = parts[0];
+    //console.log(parts[0]);
+    //console.log(today.getFullYear());
+    if((today.getFullYear()-year)<18)
+    {
+        alert("you must be 18 years old for the application");
+    }
+    else
+    {
+        return true;
+    }    
+}
+
